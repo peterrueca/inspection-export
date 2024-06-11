dbf8f4800386d8f6a941238ed0ed3c0769a707ca
Peter Rueca <peter.rueca@safetychampion.online>
2024-06-11 15:49:17 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Inspection Export
+
+Script to export questions from inspection records
+
+### Getting Started
+
+1. Create .env from .env.template
+2. Run npm i -g typescript
+3. Run npm i
+4. Run npm run start
+
---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import mongoist from 'mongoist'
+import { createObjectCsvWriter } from 'csv-writer'
+import path from 'path'
+import _ from 'lodash'
+import fs from 'fs'
+
+const db = mongoist(process.env.MONGODB_URI as string)
+const org = db[process.env.ORG as string]
+const dir = path.join(__dirname, `../tmp`)
+
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir, { recursive: true })
+}
+
+const csvWriter = createObjectCsvWriter({
+  path: `${dir}/${org}.csv`,
+  header: [
+    { id: 'c1', title: 'TEMPLATE' },
+    { id: 'c2', title: 'QUESTION' },
+  ],
+})
+
+const start = async () => {
+  console.log('Fetching records...')
+
+  const records: any[] = []
+  const cursor = await org.findAsCursor({
+    type: 'core.module.inspection',
+    archived: { $exists: false },
+    deleted: { $exists: false },
+    'details.template.sections.0': { $exists: true },
+  })
+
+  for await (const doc of cursor) {
+    const sections = _.get(doc, 'details.template.sections', [])
+
+    _.map(sections, (section) => {
+      const { questions } = section
+
+      _.map(questions, (question) => {
+        records.push({
+          c1: section.title,
+          c2: question.title,
+        })
+      })
+    })
+  }
+
+  console.log('Creating csv...')
+  await csvWriter.writeRecords(records)
+
+  console.log('Done!')
+}
+
+start()
